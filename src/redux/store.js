e12a21bfe138e2756a3408353797ff2dba7b7874
Nikolas Zec <nikolas.zec@gmail.com>
2020-05-25 12:44:09 +0200

    redux store changed

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,10 +15,7 @@ const middleware = [thunk];
 const store = createStore(
   reducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware))
 );
 
 export default store;
